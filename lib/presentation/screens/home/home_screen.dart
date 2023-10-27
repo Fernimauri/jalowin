293c8c4a0710d246f7375efbe299e59b1a74fd3c
Fernando Rodriguez <fernimauri@hotmail.com>
2023-10-26 19:36:58 -0700

    Primer commit

---
+++ lib/presentation/screens/home/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:aplicacion/config/menu/menu_items.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Aplicacion Widget')),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) => Text(appMenuItems[index].title),
+    );
+  }
+}
